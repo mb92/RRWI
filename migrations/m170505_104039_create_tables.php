4e5b06d7c11bc3f954c7b258b59d03cbc28af2bf
maciek <borowiec@devanddeliver.com>
2017-05-05 13:28:33 +0200

    1

---
+++ migrations/m170505_104039_create_tables.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+class m170505_104039_create_tables extends Migration
+{
+    public function safeUp()
+    {
+
+    }
+
+    public function safeDown()
+    {
+        echo "m170505_104039_create_tables cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m170505_104039_create_tables cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
