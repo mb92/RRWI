305956525e2535199fdeccbccba15377c91f033a
maciek <borowiec@devanddeliver.com>
2017-05-05 11:46:20 +0200

    clean install

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=selfie-ap',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
