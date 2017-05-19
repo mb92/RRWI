4076d9bd8c856968dfb5004f4be898420f722018
maciek <borowiec@devanddeliver.com>
2017-05-19 13:11:00 +0200

    merge

--- modules/admin/views/layouts/sidebar.php
+++ modules/admin/views/layouts/sidebar.php
@@ -3,10 +3,14 @@
 
     <!-- Sidebar Menu -->
     <ul class="sidebar-menu">
-        <li class="header">Country</li>
+         <li><a href="#"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
+        <li class="header">Settings</li>
         <!-- Optionally, you can add icons to the links -->
-        <li class="active"><a href="#"><i class="fa fa-link"></i> <span>Link</span></a></li>
-        <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
+        <li><a href="#"><i class="fa fa-shopping-bag"></i> <span>Stores</span></a></li>
+        <li><a href="#"><i class="fa fa-globe"></i> <span>Countries</span></a></li>
+        <li><a href="#"><i class="fa fa-flag-o"></i> <span>Languages</span></a></li>
+
+<!--         <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
         <li class="treeview">
             <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
             <span class="pull-right-container">
@@ -18,6 +22,6 @@
                 <li><a href="#">Link in level 2</a></li>
                 <li><a href="#">Link in level 2</a></li>
             </ul>
-        </li>
+        </li> -->
     </ul>
     <!-- /.sidebar-menu -->
