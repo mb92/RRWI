a4d85eec1e3b1cb4d18bf1396f97b292a11f4eac
maciek <borowiec@devanddeliver.com>
2017-05-19 13:31:08 +0200

    Move example config db

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=selfie_app',
-    'username' => 'selfie',
-    'password' => 'selfie123',
-    'charset' => 'utf8',
-];
