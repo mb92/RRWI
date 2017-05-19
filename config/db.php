4f56b5e129baa0c120b18564b4cf86f9d4032248
maciek <borowiec@devanddeliver.com>
2017-05-19 13:21:36 +0200

    Fix db.php

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=selfie-app',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=selfie_app',
+    'username' => 'selfie',
+    'password' => 'selfie123',
     'charset' => 'utf8',
 ];
