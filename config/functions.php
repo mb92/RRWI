3de2806884360324810c97d9d15d01578aa6c74d
maciek <borowiec@devanddeliver.com>
2017-06-21 13:32:33 +0200

    reworks:

--- config/functions.php
+++ config/functions.php
@@ -136,16 +136,13 @@ function remove_dir_attachment($attachPath) {
 
 
 function deleteDir($dirPath) {
-    if (! is_dir($dirPath)) {
-        throw new InvalidArgumentException("$dirPath must be a directory");
-    }
     if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
         $dirPath .= '/';
     }
     $files = glob($dirPath . '*', GLOB_MARK);
     foreach ($files as $file) {
         if (is_dir($file)) {
-            self::deleteDir($file);
+            deleteDir($file);
         } else {
             unlink($file);
         }
