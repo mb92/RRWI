06ea52e63c854bf471c8a0a07f68ac885f990042
maciek <borowiec@devanddeliver.com>
2017-08-11 09:53:53 +0200

    Reformated links for Stores HTML and T&C for email

--- models/Settings.php
+++ models/Settings.php
@@ -63,11 +63,11 @@ class Settings extends \yii\db\ActiveRecord
                 $links['share'] = 'http://instagram.com?&utm_source='.$campSource.'&utm_medium='.$campMedium.'&utm_campaign='.$campName;
                 $links['store'] = 'https://www.carphonewarehouse.com/huawei/p10.html#!colour=blue&capacity=64GB&dealType=pm&manufacturerId=2&&utm_source='.$campSource.'&utm_medium='.$campMedium.'&utm_campaign='.$campName;
 //                $links['location'] = 'https://www.carphonewarehouse.com/store-locator.html?&utm_source='.$campSource.'&utm_medium='.$campMedium.'&utm_campaign='.$campName;
-                $links['location'] = 'http://'.$_SERVER['HTTP_HOST'].'/stores-cw?&utm_source='.$campSource.'&utm_medium='.$campMedium.'&utm_campaign='.$campName;
+                $links['location'] = 'http://'.$_SERVER['HTTP_HOST'].'/stores?&utm_source='.$campSource.'&utm_medium='.$campMedium.'&utm_campaign='.$campName;
                 $links['facebook'] = 'https://www.facebook.com/huaweiuk?&utm_source='.$campSource.'&utm_medium='.$campMedium.'&utm_campaign='.$campName;
                 $links['instagram'] = 'https://www.instagram.com/huaweimobileuk?&utm_source='.$campSource.'&utm_medium='.$campMedium.'&utm_campaign='.$campName;
                 $links['twitter'] = 'https://twitter.com/huaweimobileuk?lang=en';
-                $links['terms'] = 'http://'.$_SERVER['HTTP_HOST'].'/terms-and-conditions?c=cw&utm_source='.$campSource.'&utm_medium='.$campMedium.'&utm_campaign='.$campName;
+                $links['terms'] = 'http://'.$_SERVER['HTTP_HOST'].'/terms-and-conditions&utm_source='.$campSource.'&utm_medium='.$campMedium.'&utm_campaign='.$campName;
                 $links['store-locator'] = 'https://www.carphonewarehouse.com/store-locator.html?&utm_source=selfie-app&utm_medium=email&utm_campaign=selfie';
             break;
         
