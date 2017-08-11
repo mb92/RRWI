6ad2c664123564f520b3d48e385a4e6cb1900ed9
maciek <borowiec@devanddeliver.com>
2017-08-11 11:04:16 +0200

    New email template - WIP

--- mail/cpw.php
+++ mail/cpw.php
@@ -49,7 +49,7 @@ $path = 'http://'.$_SERVER['HTTP_HOST'].'/dist/email/';
                   </tr>
                   <tr>
                     <td width="600" class="mobile" style="font-family:MyriadLight, Arial, Helvetica; font-size:12px; line-height:18px;">
-                      <img src="<?= $path ?>/imgs/women-cw2.jpg" width="100%" height="auto" style="margin:0; padding:0; border:none; display:block;" border="0" class="imgClass" alt="" />
+                      <img src="<?= $path ?>/imgs/women-cw3.jpg" width="100%" height="auto" style="margin:0; padding:0; border:none; display:block;" border="0" class="imgClass" alt="" />
                     </td>
                   </tr>
 
@@ -124,7 +124,7 @@ $path = 'http://'.$_SERVER['HTTP_HOST'].'/dist/email/';
                             To enter, all you need to do is share your Selfie Studio shot on <br/>
                             Instagram with <font style="color:#ee1c24; font-family:Myriad, Arial, Helvetica;">#P10SelfieStudio</font> and 
 
-                            <font style="color:#ee1c24; font-family:Myriad, Arial, Helvetica;">#<?= $country; ?></font> before 8th November.
+                            <font style="color:#ee1c24; font-family:Myriad, Arial, Helvetica;">#UK<?php  // echo $country; ?></font> before 8th November.
 
                           <br/><br/>
                           Each week our judge will select the image that best captures the <br/>
@@ -241,65 +241,10 @@ $path = 'http://'.$_SERVER['HTTP_HOST'].'/dist/email/';
                     </td>
                     </tr>
                     <!-- end: button discover now  -->
-
-                    <!-- grey row -->
-                    <tr bgcolor="#666666">
-                      <td class="mobile">
-                        <table width="100%" border="0" cellpadding="0" cellspacing="0">
-                        <tr>
-                          <td width="40" class="mobileOffTab"></td>
-                          <td width="16" class="mobileOnTab"></td>
-                          <td>
-                            <img src="<?= $path ?>/imgs/clean.png" width="1" height="48" style="display:block; margin:0; padding:0; border:none;"/>
-                            <center>
-                            <font style="font-size:19px; font-family:MyriadBold, Arial, Helvetica; color:#fff; letter-spacing:2px; text-align:center; font-weight:bold;" class="mobile-font">
-                            <b>HUAWEI P10 AT CARPHONE WAREHOUSE</b><br/>
-                            </font>
-                              <img src="<?= $path ?>/imgs/clean.png" width="1" height="6" style="display:block; margin:0; padding:0; border:none;"/>
-                            </center>
-                            <p style="font-family:MyriadLight, Arial, Helvetica; color:#fff; text-align:center; font-size:16px;">
-                                Make sure to order your Huawei P10 from                               
-                                Carphone Warehouse today. <br/>
-
-                                They compare the widest range of tariffs to find the right deal for you.<br/><br/>
-                              Call free 08000496103 or visit your 
-                                <a href="<?= $links['location']; ?>">
-                                    <font style="color:#001e50; font-family:Myriad, Arial, Helvetica;">
-                                        <u>local Carphone Warehouse store</u>
-                                    </font>
-                                </a><br/>
-                              to find out more.
-                            </p><br/>
-                            <img src="<?= $path ?>/imgs/clean.png" width="1" height="32" style="display:block; margin:0; padding:0; border:none;" class="mobileOffTab"/> 
-                          </td>
-                          <td width="40" class="mobileOffTab"></td>
-                          <td width="16" class="mobileOnTab"></td>
-                        </tr>
-                        </table>
-                      </td>
-                    </tr>
-
-                    <tr bgcolor="#666666">
-                      <td class="mobile" style="text-align:center;">
-                        <center>
-                        <table width="100%" border="0" cellpadding="0" cellspacing="0">
-                          <tr>
-                          <td width="178" class="mobileOffTab"></td>
-                          <td width="29" class="mobileOnTab"></td>
-                            <td width="243">
-                              <a href="<?= $links['store']; ?>">
-                              <img src="<?= $path ?>/imgs/btn-shop-now.jpg" width="243" height="54" style="display:block; margin:0; padding:0; border:none;"/>
-                              </a>
-                              <img src="<?= $path ?>/imgs/clean.png" width="1" height="72" style="display:block; margin:0; padding:0; border:none;" /> 
-                            </td>
-                          <td width="178" class="mobileOffTab"></td>
-                          <td width="29" class="mobileOnTab"></td>
-                          </tr>
-                        </table>
-                        </center>
-                      </td>
-                    </tr>
-                    <!-- end: grey row-->
+                    <?php if ($unsub != "#") { 
+                        include 'grey-row.php';
+                    }
+                    ?>
 
                     <!-- socialButtons -->
                     <tr bgcolor="#060000" height="40" background="<?= $path ?>/imgs/social-bg-2.jpg">
