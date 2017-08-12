ef6eb123b6525819086a27ae118b194de8745c6f
maciek <borowiec@devanddeliver.com>
2017-08-12 13:08:40 +0200

    Reowrks to email template

--- mail/cpw.php
+++ mail/cpw.php
@@ -265,26 +265,20 @@ $path = 'http://'.$_SERVER['HTTP_HOST'].'/dist/email/';
                       <td style="text-align:center; display:inline" class="mobile">
                         <table width="100%" border="0" cellpadding="0" cellspacing="0" align="center" >
                           <tr height="40">
-                            <td class="mobileOffTab" width="75"><img src="<?= $path ?>/imgs/social-bg.jpg" width="55" height="40" style="display:block; margin:0; padding:0; border:none;"/></td>
-                            <td class="mobileOffTab" style="color: #58595b; padding-top: 2px; font-size: 12px;">
-                                CALL&nbsp;FREE&nbsp;-&nbsp;0800&nbsp;0496103
+                            <td class="mobileOffTab">
+                              <img src="<?= $path ?>/imgs/social-bg-2.jpg" width="219" height="40" style="display:block; margin:0; padding:0; border:none;"/>
                             </td>
-      
-                            <td class="mobileOnTab" style="color: #58595b; padding-top: 4px; padding-left:4px; font-size: 14px; display:none; vertical-align:middle; font-size:12px; text-align:left;">
-                                <img src="<?= $path ?>/imgs/clean.png" width="1" height="2px" style="display:block; margin:0; padding:0; border:none;" />
-                                CALL FREE:<br/>0800&nbsp;0496103
-                            </td>
-                            <td width="55">
-                              <a href="<?= $links['instagram']; ?>"><img src="<?= $path ?>/imgs/icon_instagram.jpg" width="55" height="40" style="display:block; margin:0; padding:0; border:none;"/></a>
+                            <td width="59">
+                              <a href="<?= $links['instagram']; ?>"><img src="<?= $path ?>/imgs/icon_instagram-2.jpg" width="59" height="40" style="display:block; margin:0; padding:0; border:none;"/></a>
                             </td>
                             <td width="47">
-                              <a href="<?= $links['facebook']; ?>"><img src="<?= $path ?>/imgs/icon_fb.jpg" width="47" height="40" style="display:block; margin:0; padding:0; border:none;"/></a>
+                              <a href="<?= $links['facebook']; ?>"><img src="<?= $path ?>/imgs/icon_fb-2.jpg" width="47" height="40" style="display:block; margin:0; padding:0; border:none;"/></a>
                             </td>
-                            <td width="57">
-                              <a href="<?= $links['twitter']; ?>"><img src="<?= $path ?>/imgs/icon_twitter.jpg" width="57" height="40" style="display:block; margin:0; padding:0; border:none;"/></a>
+                            <td width="51">
+                              <a href="<?= $links['twitter']; ?>"><img src="<?= $path ?>/imgs/icon_twitter-2.jpg" width="51" height="40" style="display:block; margin:0; padding:0; border:none;"/></a>
                             </td>
                             <td class="mobileOffTab">
-                              <img src="<?= $path ?>/imgs/social-bg.jpg" width="190" height="40" style="display:block; margin:0; padding:0; border:none;"/>
+                              <img src="<?= $path ?>/imgs/social-bg-2.jpg" width="224" height="40" style="display:block; margin:0; padding:0; border:none;"/>
                             </td>
                           </tr>
                         </table>
